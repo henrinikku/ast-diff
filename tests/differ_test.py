c7d895f825b3ea78ba37ace193bd3d71a3dcc003
Henri Nikku <henrinikku1@gmail.com>
2022-10-20 19:44:50 +0300

    Test diff with empty files

--- tests/differ_test.py
+++ tests/differ_test.py
@@ -5,6 +5,22 @@ from astdiff.parser import ParsoParser
 def test_diff(parser: ParsoParser):
     source_ast = parser.parse_code("print('123')")
     target_ast = parser.parse_code("print('321')")
-    edit_script = diff(source_ast, target_ast)
+    context = diff(source_ast, target_ast)
 
-    assert bool(edit_script)
+    assert bool(context.edit_script)
+
+
+def test_diff_empty_file(parser: ParsoParser):
+    source_ast = parser.parse("tests/data/empty.py")
+    target_ast = parser.parse("tests/data/empty.py")
+    context = diff(source_ast, target_ast)
+
+    assert not context.edit_script
+
+
+def test_diff_empty_and_whitespace(parser: ParsoParser):
+    source_ast = parser.parse("tests/data/empty.py")
+    target_ast = parser.parse("tests/data/whitespace.py")
+    context = diff(source_ast, target_ast)
+
+    assert not context.edit_script
